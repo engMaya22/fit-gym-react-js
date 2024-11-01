ddd14d1210eb06d1ad9dbd28d326b0f84f778df2
maya <maya.e@webased.net>
2024-11-01 01:07:55 -0700

    hero section style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 
 export default function Hero() {
   return (
-    <div>
-      hero
+    <div className="min-h-screen flex flex-col items-center justify-center gap-10 text-center 
+            mx-auto w-full max-w-[800px]">
+       <div className="gap-4 flex flex-col">
+       <p>IT'S TIME TO GET</p>
+       <h1 className="mr-4 upercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">Swole<span className="text-blue-400">nurmous</span></h1>
+       </div>
+       <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local <span className='text-blue-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+
+        <button className="rounded-md px-8 py-4 blueShadow border-[2px] bg-slate-950 border-blue-400 border-solid duration-200">
+          <p> Accept && Begin</p>
+        </button>
+
     </div>
   )
 }
