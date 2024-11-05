92da7331f11169b279e0971393f6da1cc2f96d63
maya <maya.e@webased.net>
2024-11-05 06:54:19 -0800

    format

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -10,13 +10,13 @@ const ExerciseCard = ({exercise , idx})=>{
 
     }
     return  <div className="bg-slate-950 flex flex-col p-4 sm:flex-wrap rounded-md"> 
+    
                 <div className="flex flex-col sm:flex-row sm:flex-wrap sm:items-center gap-x-4">
                     <h4 className="hidden sm:inline text-3xl sm:text-4xl md:text-5xl font-smibold text-slate-400">
                       0{idx + 1}
                     </h4>
                     <h2 className='capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center'>{exercise.name.replaceAll("_", " ")}</h2>
                     <p className="captialize text-sm text-slate-400">{exercise.type}</p>
-
                 </div>
 
                 <div className="flex flex-col">
@@ -39,7 +39,7 @@ const ExerciseCard = ({exercise , idx})=>{
                   {
                     ['reps','rest','tempo'].map(info => {
                       return(
-                              <div key={info} className="rounded border-[1.5px] flex flex-col p-2 w-full border-solid border-slate-900">
+                              <div key={info} className="rounded border-[1.5px] flex flex-col p-2 mt-2 w-full border-solid border-slate-900">
                                   <h3 className="text-slate-400 text-sm captialize"> {info === 'reps' ?  `${exercise.unit}` : info}</h3>
                                   <p className="font-medium">{exercise[info]}</p>
                               </div>
@@ -47,12 +47,13 @@ const ExerciseCard = ({exercise , idx})=>{
                     })
 
                   }
-                  <button onClick={handleSetIncrement} className="flex flex-col p-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full">
+                  <button onClick={handleSetIncrement} className="flex flex-col p-2 mt-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full">
                     <h3 className='text-slate-400 text-sm capitalize'>Sets completed</h3>
                     <p className='font-medium'> {setsComplete} / 5</p>
                   </button>
 
                 </div>
+
             </div>
 }
 
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -14,9 +14,6 @@ export default function Workout({workout}) {
        }
 
       </div>
-
-
-
     </SectionWrapper>
   )
 }
